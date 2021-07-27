b5ba817a688d9d5a8ac964a93fc269056f8316f9
notch986 <diegohh1998@gmail.com>
2021-07-27 01:05:52 -0500

    agregando dependencias

---
+++ dependencias.php
@@ -0,0 +1,14 @@
+<link rel="stylesheet" type="text/css" href="public/bootstrap4/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="public/fontawesome/css/all.css">
+<link rel="stylesheet" href="public/datatable/bootstrap.css">
+<link rel="stylesheet" href="public/datatable/dataTables.bootstrap4.min.css">
+
+
+<script src="public/jquery-3.6.0.min.js"></script>
+<script src="public/bootstrap4/popper.min.js"></script>
+<script src="public/bootstrap4/bootstrap.min.js"></script>
+<script src="public/datatable/jquery.dataTables.min.js"></script>
+<script src="public/datatable/dataTables.bootstrap4.min.js"></script>
+<script src="public/sweetalert.min.js"></script>
+
+<link rel="icon" href="public/img/logo-unsa.png" type="image/png">
